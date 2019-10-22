5b5e5d1a72f4fab0358694e3ce4be50ba7cbb9d4
yandaly <thegreygringo@gmail.com>
2019-10-22 15:05:04 +0200

    router-links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,7 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
-      { path: '', component: ProductDetailsComponent },
+      { path: 'products/:productId', component: ProductDetailsComponent },
     ])
   ],
   declarations: [
