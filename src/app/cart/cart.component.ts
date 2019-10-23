d7dc02421188893464856aff5e2d72bf1e36a353
yandaly <thegreygringo@gmail.com>
2019-10-23 17:56:05 +0200

    fin d'intégration des form et fin du tuto angular

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../cart.service';
 
-import { CartService } from '../cart.service';
 import { FormBuilder } from '@angular/forms';
 
 @Component({
@@ -18,11 +17,24 @@ export class CartComponent implements OnInit {
   constructor(
     private cartService: CartService,
     private formBuilder: FormBuilder
-  ) { }
+  ) {
+    this.checkoutForm = this.formBuilder.group({
+      name: '',
+      address: '',
+    });
+
+    this.count = this.cartService.getCount();
+    this.price = this.cartService.getTotalPrice();
+  }
 
   ngOnInit() {
     this.items = this.cartService.getItems();
-    this.count = this.cartService.getCount();
-    this.price = this.cartService.getTotalPrice();
+  }
+
+  onSubmit(customerData) {
+    console.warn('Your order has been submitted', customerData);
+
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
   }
 }
